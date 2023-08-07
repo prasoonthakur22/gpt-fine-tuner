e29b1ceeabd878ec042092f573f46b3a97411be3
Prasoon Thakur <prasoonthakur22@gmail.com>
2023-08-07 16:51:14 +0530

    Final Code

---
+++ app/trainer/key/page.js
@@ -0,0 +1,11 @@
+const SecretKey = () => {
+  return (
+    <div className="min-h-screen bg-gray-900 text-white flex items-center justify-center">
+      <div className="w-full max-w-md">
+        <h1 className="text-4xl font-bold mb-8 text-center">Enter Your OpenAI Secret Key</h1>
+      </div>
+    </div>
+  );
+};
+
+export default SecretKey;
